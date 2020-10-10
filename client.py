4d3065e49ad34bb88c9624e530b49c5cc32f115d
Alex Lim <alexanderheionlim@gmail.com>
2020-10-10 16:05:53 -0400

    client first pass with sample data

---
+++ client.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import requests
+
+url = "https://46y7hjo0yf.execute-api.us-east-2.amazonaws.com/dev/test"
+
+# low_memory -> ensure no mixed dtypes
+data = pd.read_csv("sample.csv", sep=",", low_memory=False)
+
+# Write all the rows from sample data to the api as a post request
+for i in data.index:
+    try:
+        export = data.loc[i].to_json()
+        response = requests.post(url, data=export)
+        print(export)
+        print(response)
+    except ValueError:
+        print(data.loc[i])
