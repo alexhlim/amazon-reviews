2a56482a3c71e2fb3c0eb39f9e637fa02998c8dc
Alex Lim <alexanderheionlim@gmail.com>
2020-11-08 14:03:59 -0500

    spark shell script v1

--- bin/run.sh
+++ bin/run.sh
@@ -3,5 +3,10 @@ export APP_NAME=AmazonReviews
 export STREAM_NAME=AmazonReviews-APIData
 export ENDPOINT=https://kinesis.us-east-2.amazonaws.com
 export DYNAMODB_TABLE=AmazonReviews-Product
+export SPARK_PATH=~/Documents/dev/spark-3.0.1-bin-hadoop3.2
+export AMAZON_REVIEWS_PATH=~/Documents/dev/amazon-reviews
 
-exec sbt "run ${APP_NAME} ${STREAM_NAME} ${ENDPOINT} ${DYNAMODB_TABLE}"
+sbt clean assembly && \
+"${SPARK_PATH}/bin/spark-submit" \
+"${AMAZON_REVIEWS_PATH}/target/scala-2.12/amazon-reviews-streaming-assembly-0.1.jar" \
+${APP_NAME} ${STREAM_NAME} ${ENDPOINT} ${DYNAMODB_TABLE}
